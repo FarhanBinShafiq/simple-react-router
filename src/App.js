b695fef0207aff590870a67a407df50830bee457
FarhanBinShafiq <farhan.shafiq@northsouth.edu>
2021-12-18 01:07:50 +0600

    routing Prac

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Friends from './components/Friends/Friends';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+import {  } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter as  Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Frienddetails from './components/Friend-details/Frienddetails';
+import Culture from './components/Culture/Culture';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       
+         <Router>
+           <Header></Header>
+           <Switch>
+            <Route path="/home"><Home></Home></Route>
+             <Route path="/friends"><Friends></Friends></Route>
+             <Route path="/friend/:frienddetail"><Frienddetails></Frienddetails></Route>
+             <Route exact path="/about"><About></About></Route>
+             <Route exact path="/about/culture"><Culture></Culture></Route>
+              <Route path="/"><NotFound></NotFound></Route>
+             
+           </Switch>
+         </Router>
+     
+     
     </div>
   );
 }
